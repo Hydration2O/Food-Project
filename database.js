539db35e00e5871bb90f8599d5c76b229ffa54eb
Abdullah Mashaan <eng.aalenezi@gmail.com>
2024-12-09 19:52:28 +0300

    mongo String Added

--- database.js
+++ database.js
@@ -4,7 +4,9 @@ const connectDB = async () => {
   console.log("here");
 
   try {
-    const conn = await mongoose.connect("");
+    const conn = await mongoose.connect(
+      "mongodb+srv://foodproject1900:ILikeFood0091@cluster0.7mlg2.mongodb.net/"
+    );
     console.log(`mongo connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
