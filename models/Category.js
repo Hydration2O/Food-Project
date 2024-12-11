7bb1a88ee1b00aef9a3ea7ff9b708b61a870696b
Q8AlKhars <Mohammad.a.alkhars@gmail.com>
2024-12-11 17:37:17 +0300

    CategorySchema done

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,8 @@
 const mongoose = require("mongoose");
 
 const CategorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    image: String,
-    required: true,
-    unique: true,
-  },
+  name: String,
+  image: String,
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
